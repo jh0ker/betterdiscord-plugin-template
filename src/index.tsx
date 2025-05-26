13e30c707d39bab6ac80a569d9f9e4b04d7325db
Jannes Höke <mail@jhoeke.de>
2025-05-26 14:01:33 +0200

    Use types from `zerthox/betterdiscord-types` for plugin definition

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,21 @@
 import React from 'react';
-import { Plugin } from './bd';
+import type { Meta, Plugin, PluginClass } from 'betterdiscord';
 import { BoundBdApi } from 'betterdiscord';
 
-module.exports = class extends Plugin {
+module.exports = class implements Plugin {
+  private meta: Meta;
   private bdApi: BoundBdApi;
 
-  constructor() {
-    super();
+  constructor(meta: Meta) {
+    this.meta = meta;
     this.bdApi = new BdApi('TemplatePlugin');
   }
 
-  protected start(): void {
+  start(): void {
     this.bdApi.UI.alert('Hello World!', <div>Hello World!</div>);
   }
 
-  protected stop(): void {
+  stop(): void {
     this.bdApi.Patcher.unpatchAll();
   }
-};
+} satisfies PluginClass;  // Ensures BetterDiscord gets the expected constructor signature
